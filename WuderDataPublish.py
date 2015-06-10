81376a998e16f7a82efaba3a7165f6d13916b5c3
Gaelen McFadden <gaelen@mcfaddencentral.com>
2015-06-10 09:06:55 -0500

    Json Publish

--- WuderDataPublish.py
+++ WuderDataPublish.py
@@ -32,31 +32,52 @@ def main():
 	mqttc.connect("10.3.12.48", 1883, 60)
 	mqttc.loop_start()
 	lasttime = []
+	count = 0
 	while 0 < 1:
-		rep = loadStation(url)
-		try:
+		if (count == 0):
+			rep = loadStation(url)
 			temp = parseTemp(rep)
 			thime = parseTime(rep)
-			if thime != lasttime:
-				#publish.single("/SanAntonioTemp/Wunder", payload=("At "+str(thime)+" the temp was "+str(temp)),qos=2,hostname="10.0.0.3")
-				#publish.single("/SanAntonioTemp/Wunder", payload=("At "+str(thime)+" the temp was "+str(temp)),qos=2,hostname="10.3.12.48", port=1883)
-				#mqttc.publish("/SanAntonioTemp/Wunder", (str(thime)+ ' the temperature was ' + str(temp)), qos=2)
+			try:
 				mqttc.publish("/WundergroundSA", rep, qos=2)
 				mqttc.publish("/WundergroundSA/count", "0", qos=2)
-				lasttime = thime
-				count = 0
-				print "At "+str(thime)+ " the temp was "+str(temp)+ " has been published"  
-			else:
-				count = count + 60*4
-				#publish.single("/SanAntonioTemp/Wunder", payload=("Time last updated was "+str(count/60)+ " minutes ago and the temperature was "+str(temp)),qos=2,hostname="10.0.0.3")
-				#publish.single("/SanAntonioTemp/Wunder", payload=("Time last updated was "+str(count/60)+ " minutes ago and the temperature was "+str(temp)),qos=2,hostname="10.3.12.48", port=1883)
-				#mqttc.publish("/SanAntonioTemp/Wunder", ('Last updated '+str(count/60)+" minutes ago and the temperature was "+str(temp)), qos=2)
+				count = -1;
+				print 'Updated Weather Data'
+			except:
+				print 'Could not parse data. Will retry in 1 second.'
+				count = -1;
+		else:
+			try:
 				mqttc.publish("/WundergroundSA", rep, qos=2)
-				mqttc.publish("/WundergroundSA/count", count, qos=2)
-				print "Published new count " +str(count/60)
-		except:
-			print 'Could not parse data. Will retry in 15 seconds.'
-		time.sleep(60*4)
+				mqttc.publish("/WundergroundSA/count", str(count), qos=2)
+				print 'Republished Old Data. The count is '+ str(count)+' seconds.'
+			except:
+				print 'Could not publish old data. Will attempt to get new data in 1 second.'
+				count = -1
+		count = count +1
+		if count == (60*4):
+			count = 0
+		time.sleep(1)
+			
+#			if thime != lasttime:
+#				#publish.single("/SanAntonioTemp/Wunder", payload=("At "+str(thime)+" the temp was "+str(temp)),qos=2,hostname="10.0.0.3")
+#				#publish.single("/SanAntonioTemp/Wunder", payload=("At "+str(thime)+" the temp was "+str(temp)),qos=2,hostname="10.3.12.48", port=1883)
+#				#mqttc.publish("/SanAntonioTemp/Wunder", (str(thime)+ ' the temperature was ' + str(temp)), qos=2)
+#				
+#				lasttime = thime
+#				count = 0
+#				print "At "+str(thime)+ " the temp was "+str(temp)+ " has been published"  
+#			else:
+#				count = count + 60*4
+#				#publish.single("/SanAntonioTemp/Wunder", payload=("Time last updated was "+str(count/60)+ " minutes ago and the temperature was "+str(temp)),qos=2,hostname="10.0.0.3")
+#				#publish.single("/SanAntonioTemp/Wunder", payload=("Time last updated was "+str(count/60)+ " minutes ago and the temperature was "+str(temp)),qos=2,hostname="10.3.12.48", port=1883)
+#				#mqttc.publish("/SanAntonioTemp/Wunder", ('Last updated '+str(count/60)+" minutes ago and the temperature was "+str(temp)), qos=2)
+#				mqttc.publish("/WundergroundSA", rep, qos=2)
+#				mqttc.publish("/WundergroundSA/count", count, qos=2)
+#				print "Published new count " +str(count/60)
+#		except:
+#			print 'Could not parse data. Will retry in 15 seconds.'
+#		time.sleep(60*4)
 
 
  
